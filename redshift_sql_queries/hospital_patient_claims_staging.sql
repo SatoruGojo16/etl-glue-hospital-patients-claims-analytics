5608663ec0d31c7bed15a01143d88c43980c26b2
Satoru Gojo <sakthisanthosh56.8@gmail.com>
2026-02-01 20:36:23 +0530

    Redshift SQL Queries Update

--- redshift_sql_queries/hospital_patient_claims_staging.sql
+++ redshift_sql_queries/hospital_patient_claims_staging.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS staging_patient;
 CREATE TABLE staging_patient (
   patient_id varchar,
-  name varchar,
+  name_prefix varchar,
+  first_name varchar,
+  last_name varchar,
+  patient_full_name varchar,
   date_of_birth varchar,
   phone_number varchar,
   email_id varchar
@@ -11,7 +14,7 @@ DROP TABLE IF EXISTS staging_policy;
 CREATE TABLE staging_policy (
   policy_id varchar,
   policy_start_date varchar,
-  policy_expire_date varchar,
+  policy_end_date varchar,
   premium_amount varchar,
   coverage_limit varchar
 );
@@ -19,16 +22,23 @@ CREATE TABLE staging_policy (
 DROP TABLE IF EXISTS staging_address;
 CREATE TABLE staging_address (
   address_id varchar,
+  addressline varchar,
   borough varchar,
-  region varchar,
-  latitude varchar,
-  longitude varchar
+  borough_level varchar,
+  borough_latitude varchar,
+  borough_longitude varchar,
+  borough_abbrev varchar,
+  borough_code varchar,
+  city varchar,
+  state varchar
 );
 
 DROP TABLE IF EXISTS staging_claims;
 CREATE TABLE staging_claims (
   claim_id varchar,
+  high_risk_claim_flag  varchar,
   claim_initialized_date varchar,
   claim_request_amount varchar,
-  claim_status varchar
+  claim_status varchar,
+  claim_rejected_reason varchar
 );
